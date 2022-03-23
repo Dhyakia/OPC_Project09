f040a95efb27b9b6558f09012324b6d355d9470b
Rimonteil <nardiotencha@gmail.com>
2022-03-23 13:17:21 +0100

    wireframe: flux (3/10)

--- LITReview/LITReview/urls.py
+++ LITReview/LITReview/urls.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.urls import path
 
 from authentication import views as authV
-from review import views
+from review import views as reviV
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/login/', authV.login, name='login'),
     path('auth/signup/', authV.signup, name='signup'),
+    path('flux/', reviV.flux, name='flux'),
 ]
--- LITReview/review/views.py
+++ LITReview/review/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def flux(request):
+    return render(
+        request,
+        'review/flux.html'
+    )
