f93a683e6806b6d2616c36f58f063100bc54a9ab
Rimonteil <nardiotencha@gmail.com>
2022-04-01 15:29:19 +0200

    review form created

--- LITReview/review/forms.py
+++ LITReview/review/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from review.models import Ticket
+from review.models import Ticket, Review
 
 
 class TicketForm(forms.ModelForm):
@@ -7,3 +7,10 @@ class TicketForm(forms.ModelForm):
     class Meta:
         model = Ticket
         fields = ("title", "description", "image")
+
+
+class ReviewForm(forms.ModelForm):
+
+    class Meta:
+        model = Review
+        fields = ("title", "rating", "body")
